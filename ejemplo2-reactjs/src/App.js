021289a256492d847c45ec4e3e3817be276dbbed
ErikGa25 <jerik67@gmail.com>
2021-12-01 11:06:08 -0600

    propTypes

--- ejemplo2-reactjs/src/App.js
+++ ejemplo2-reactjs/src/App.js
@@ -4,6 +4,7 @@ import ConditionalSection from './sections/conditional';
 import cars from './data/cars.json';
 import {Component} from 'react';
 import Forms from './sections/forms';
+import PropTypes from 'prop-types';
 
 class CarItem extends Component {
   render() {
@@ -49,6 +50,49 @@ class Events extends Component {
   }
 }
 
+class Box extends Component {
+  render() {
+    return(
+      <div style={{border:'1px solid #000', margin:5,padding:5}}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+class Article extends Component {
+  //validar los tipos de dato
+  static propTypes = {
+    author: PropTypes.string.isRequired
+  }
+
+  /*constructor(props) {
+    super(props);
+
+    if(typeof props.author === 'undefined') {
+      console.warn('author es requrido');
+      throw new Error('author es requerido');
+    }
+  }*/
+  render () {
+    const {author, title, date, children} = this.props;
+    return (
+      <section>
+        <h2>{title}</h2>
+        {author && <p><em>Escrito por {author}</em></p>}
+        <Box>{date}</Box>
+        <article>
+          {children}
+        </article>
+      </section>
+    )
+  }
+}
+
+// forma de agregar las prototypes, no recomendada
+/*Article.propTypes = {
+  author: PropTypes.string
+}*/
+
 function App() {
   const numbers = [1, 1, 3, 4, 5];
 
@@ -76,6 +120,37 @@ function App() {
         <Events />
 
         <Forms />
+
+        <h4>children props</h4>
+        <Box>Hola soy un children</Box>
+        <Box>Otro Box</Box>
+
+        <Article
+          author='Miguel'
+          date={new Date().toLocaleDateString()}
+          title='Artículo sobre la prop children'
+        >
+          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children.</p>
+          <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
+
+        <Article
+          author={1111}
+          date={new Date().toLocaleDateString()}
+          title='Artículo 2'
+        >
+          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children.</p>
+          <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
+        
+        <Article
+          author='Miguel'
+          date={new Date().toLocaleDateString()}
+          title='Otro artículo'
+        >
+          <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children.</p>
+          <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
       </header>
     </div>
   );
